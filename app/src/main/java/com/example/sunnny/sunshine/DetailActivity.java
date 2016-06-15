5775c9755d4449c059499437b709759446a1370a
Sunny <sun3ysaini@gmail.com>
2016-06-15 15:04:49 +0530

    Added Cursor loader to detail and main activity

--- app/src/main/java/com/example/sunnny/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/sunnny/sunshine/DetailActivity.java
@@ -1,6 +1,9 @@
 package com.example.sunnny.sunshine;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,7 +12,38 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class DetailActivity extends AppCompatActivity {
+import com.example.sunnny.sunshine.data.WeatherContract;
+
+public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+
+
+    private static final String LOG_TAG = DetailActivity.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+
+    private String mForecast;
+
+    private static final int DETAIL_LOADER = 0;
+
+    private static final String[] FORECAST_COLUMNS = {
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+    };
+
+    // these constants correspond to the projection defined above, and must change if the
+    // projection changes
+    private static final int COL_WEATHER_ID = 0;
+    private static final int COL_WEATHER_DATE = 1;
+    private static final int COL_WEATHER_DESC = 2;
+    private static final int COL_WEATHER_MAX_TEMP = 3;
+    private static final int COL_WEATHER_MIN_TEMP = 4;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +51,8 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getLoaderManager().initLoader(DETAIL_LOADER,savedInstanceState, this);
 
-        Intent i=getIntent();
-        String data=i.getStringExtra(Intent.EXTRA_TEXT);
-        TextView t=(TextView)findViewById(R.id.weatherText);
-        t.setText(data);
     }
 
     public boolean onCreateOptionsMenu(Menu menu)
@@ -44,4 +75,57 @@ public class DetailActivity extends AppCompatActivity {
         return true;
     }
 
+
+    @Override
+    public android.content.Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        Intent intent = getIntent();
+        if (intent == null) {
+            return null;
+        }
+
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        return new CursorLoader(
+                getApplicationContext(),
+                intent.getData(),
+                FORECAST_COLUMNS,
+                null,
+                null,
+                null
+        );
+
+    }
+
+    @Override
+    public void onLoadFinished(android.content.Loader<Cursor> loader, Cursor data) {
+
+        if (!data.moveToFirst()) { return; }
+
+        String dateString = Utility.formatDate(
+                data.getLong(COL_WEATHER_DATE));
+
+        String weatherDescription =
+                data.getString(COL_WEATHER_DESC);
+
+        boolean isMetric = Utility.isMetric(getApplicationContext());
+
+        String high = Utility.formatTemperature(
+                data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+
+        String low = Utility.formatTemperature(
+                data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+
+        mForecast = String.format("%s - %s - %s/%s", dateString, weatherDescription, high, low);
+
+       // Log.v("Sunny",mForecast);
+        TextView detailTextView = (TextView)findViewById(R.id.weatherText);
+        detailTextView.setText(mForecast);
+
+
+    }
+
+    @Override
+    public void onLoaderReset(android.content.Loader<Cursor> loader) {
+
+    }
 }
--- app/src/main/java/com/example/sunnny/sunshine/MainActivity.java
+++ app/src/main/java/com/example/sunnny/sunshine/MainActivity.java
@@ -1,66 +1,104 @@
 package com.example.sunnny.sunshine;
 
+import android.app.LoaderManager;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import com.example.sunnny.sunshine.data.WeatherContract;
 
-public class MainActivity extends AppCompatActivity {
 
-    ArrayAdapter<String> adapter;
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int FORECAST_LOADER = 0;
+    ForecastAdapter adapter;
+    //String mLocation;
+
+    private static final String[] FORECAST_COLUMNS = {
+            // In this case the id needs to be fully qualified with a table name, since
+            // the content provider joins the location & weather tables in the background
+            // (both have an _id column)
+            // On the one hand, that's annoying.  On the other, you can search the weather table
+            // using the location set by the user, which is only in the Location table.
+            // So the convenience is worth it.
+            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.COLUMN_DATE,
+            WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
+            WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
+            WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+            WeatherContract.LocationEntry.COLUMN_LOCATION_SETTING,
+            WeatherContract.WeatherEntry.COLUMN_WEATHER_ID,
+            WeatherContract.LocationEntry.COLUMN_COORD_LAT,
+            WeatherContract.LocationEntry.COLUMN_COORD_LONG
+    };
+
+    // These indices are tied to FORECAST_COLUMNS.  If FORECAST_COLUMNS changes, these
+    // must change.
+    static final int COL_WEATHER_ID = 0;
+    static final int COL_WEATHER_DATE = 1;
+    static final int COL_WEATHER_DESC = 2;
+    static final int COL_WEATHER_MAX_TEMP = 3;
+    static final int COL_WEATHER_MIN_TEMP = 4;
+    static final int COL_LOCATION_SETTING = 5;
+    static final int COL_WEATHER_CONDITION_ID = 6;
+    static final int COL_COORD_LAT = 7;
+    static final int COL_COORD_LONG = 8;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
+        adapter = new ForecastAdapter(getApplicationContext(),null, 0);
 
-
-
-        adapter=new ArrayAdapter<String>(getApplicationContext(),R.layout.list_item_forcast,R.id.listitem_forcast_text,new ArrayList<String>());
         ListView listView=(ListView)findViewById(R.id.listview_forcast);
         listView.setAdapter(adapter);
+
+        getLoaderManager().initLoader(FORECAST_LOADER,savedInstanceState,this);
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-                String data=adapter.getItem(i);
-                Intent intent=new Intent(getApplicationContext(),DetailActivity.class).putExtra(Intent.EXTRA_TEXT,data);
-                startActivity(intent);
+                Cursor cursor=(Cursor)adapterView.getItemAtPosition(i);
 
-
+                if(cursor!=null)
+                {
+                    String locationSettings=Utility.getPreferredLocation(getApplicationContext());
+                    Intent intent=new Intent(getApplicationContext(),DetailActivity.class).setData(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(locationSettings,cursor.getLong(COL_WEATHER_DATE)));
+                    startActivity(intent);
+                }
             }
         });
 
-        
-
     }
+
 
    public void updateWeather()
     {
-        FetchWeatherTask fatchWeatherTask=new FetchWeatherTask(getApplicationContext(),adapter);
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-        String location = prefs.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default));
+        FetchWeatherTask fatchWeatherTask=new FetchWeatherTask(getApplicationContext());
+        String location =Utility.getPreferredLocation(getApplicationContext());
         fatchWeatherTask.execute(location);
-    }
-
-
-    public void onStart()
-    {
-        super.onStart();
-        updateWeather();
 
     }
+
+
+//    public void onStart()
+//    {
+//        super.onStart();
+//        updateWeather();
+//
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
@@ -87,6 +125,30 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
+
+
+
+    @Override
+    public android.content.Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        String locationSettings=Utility.getPreferredLocation(getApplicationContext());
+
+        String sortOrder= WeatherContract.WeatherEntry.COLUMN_DATE+" ASC";
+        Uri weatherForLocationUri=WeatherContract.WeatherEntry.buildWeatherLocationWithStartDate(locationSettings,System.currentTimeMillis());
+        return new android.content.CursorLoader(getApplicationContext(),weatherForLocationUri,FORECAST_COLUMNS,null,null,sortOrder);
+
+    }
+
+    @Override
+    public void onLoadFinished(android.content.Loader<Cursor> loader, Cursor cursor) {
+        adapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(android.content.Loader<Cursor> loader) {
+        adapter.swapCursor(null);
+    }
+
+
 
 
 }
